171d802317724a0cf5ed976969b8e4e33c50c705
phiyadawave <Phiyada.ra.67@ubu.ac.th>
2025-01-24 14:14:12 +0700

    code สุดเทพ

---
+++ app.py
@@ -0,0 +1,12 @@
+class Student(object):
+  student_count = 0
+
+def _new_(self):
+  print('Studeny.__new__')
+
+def __init__(self):
+  print('Student.__init__')
+  self.gpa = 4.0
+
+def study_hard(self):
+  print('sir, yes sir.')
